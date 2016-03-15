9ba2ee7cefbe00fb3df17e4d5bbf476bf20ff373
floydbe <bef2cj@virginia.edu>
2016-03-15 17:16:51 -0400

    updated version of main

--- main.py
+++ main.py
@@ -3,120 +3,127 @@ import sys
 import os
 import getopt
 
-
 class Armor():
 	def __init__(self):
-		self.master = {}
-
-	def add(self, name, t, d, w):
-		self.master[name] = (t, d, w)
+		self.master_helms = {}
+		self.master_chests = {}
+		self.master_gauntlets = {}
+		self.master_greaves = {}
 	
-	def get_type(self, t):
-		return {k:v for k,v in self.master.items() if v[0]==t}
+	def add(self, t, name, d, w):
+		if t == "helms":
+			self.master_helms[name] = (d,w)
+		elif t == "chests":
+			self.master_chests[name] = (d,w)
+		elif t == "gauntlets":
+			self.master_gauntlets[name] = (d,w)
+		elif t == "greaves":
+			self.master_greaves[name] = (d,w)
 
-		
-def add_item(item, t):
-	print "Entering stats for %s." % item
-	defense = raw_input("Defense: ")
-	weight = raw_input("Weight: ")
-	with open("DS3_%s.csv" % t, "a") as f:
-		f.write("%s,%s,%s%s" % (item, defense, weight, os.linesep))
-	print item, "has been added to DS3_%s.csv" % t
-	return defense, weight
+class Main():
+	def __init__(self, args):
+		self.args = args
+		self.user = None
+		self.parse_args()
+		self.owned_file = "%s_owned.txt" % self.user
+		if not os.path.isfile(self.owned_file): # make sure owned file exists
+			self.create_owned_file()
+			sys.exit()
 
-def build_armor_dict(a, l, t):
-# a is an Armor object
-# l is list of item name strings
-# t is string of item type
-	with open("DS3_%s.csv" % t, "r+") as f:
-		master_list = f.read()
-	for item in l:
-		if item in master_list:
-			d = master_list[master_list.find(item)+len(item)+1] # this is super hacky...
-			w = master_list[master_list.find(item)+len(item)+3]
-			a.add(item, t, d, w)
+		self.owned_helms = []
+		self.owned_chests = []
+		self.owned_gauntlets = []
+		self.owned_greaves = []
+		try:
+			self.read_owned_file()
+		except IOError:
+			print "Error: something went wrong reading", self.owned_file
+			sys.exit()
+		self.master = Armor()
+		self.build_master()
+		print self.master.master_helms
+
+	def build_master(self):
+		lists = [self.owned_helms, self.owned_chests, self.owned_gauntlets, self.owned_greaves]
+		types = ["helms","chests","gauntlets","greaves"]
+		for i in range(4):
+			with open("DS3_%s.csv" % types[i], "r+") as f:
+				master_list = f.read()
+			for item in lists[i]:
+				if item in master_list:
+					d = master_list[master_list.find(item)+len(item)+1] # this is super hacky
+					w = master_list[master_list.find(item)+len(item)+3]
+					self.master.add(types[i],item,d,w)
+				else:
+					print item, "does not exist in master list.",
+					add = raw_input("Would you like to interactively add it? ")
+					if add.strip() in ["yes",'y']:
+						d,w = self.add_item(item, types[i])
+						self.master.add(types[i], item, d, w)
+					else:
+						print item, "must be intered in master list to continue."
+						sys.exit()
+
+	def add_item(self, item, t):
+		print "Entering stats for %s." % item
+		defense = raw_input("Defense: ")
+		weight = raw_input("Weight: ")
+		if defense.isdigit() and weight.isdigit():
+			with open("DS3_%s.csv" % t, "a") as f:
+				f.write("%s,%s,%s%s" % (item, defense, weight, os.linesep))
+			print item, "has been added to DS3_%s.csv" % t
+			return defense, weight
 		else:
-			print item, "does not exist in master list.",
-			add = raw_input("Would you like to interactively add it? ")
-			if add.strip() in ["yes","y"]:
-				d, w = add_item(item, t)
-				a.add(item, t, d, w)
-			else:
-				print item, "must be entered in master list to continue."
-				sys.exit()
-
-def read_owned_file(owned_file):
-	helms = []
-	chests = []
-	gauntlets = []
-	greaves = []
-	with open(owned_file, 'r') as f:
-		for line in f:
-			l = line.strip()
-			if l[0] == '#':
-				category = l[1:]
-			elif category == "HELMS":
-				helms.append(l)
-			elif category == "CHESTS":
-				chests.append(l)
-			elif category == "GAUNTLETS":
-				gauntlets.append(l)
-			elif category == "GREAVES":
-				greaves.append(l)
-			else:
-				raise IOError
-	return helms,chests,gauntlets,greaves
-
-def create_owned_file(owned_file):
-	create = raw_input("%s was not found. Would you like it to be created for you? " % owned_file)
-	if create in ["yes","y"]:
-		with open(owned_file, "w") as f:
-			f.write("#HELMS{0}No Helm{0}#CHESTS{0}No Chest{0}#GAUNTLETS{0}No Gauntlets{0}#GREAVES{0}No Greaves".format(os.linesep))
-		print owned_file, "has been created."
-	else:
-		print "Cannot continue without", owned_file
-
-
-def usage():
-	print "USAGE NOT YET IMPLETMENTED"
-
-def main(argv):
-	try:
-		opts,args = getopt.getopt(argv, "hu:", ["help","user="])
-	except getopt.GetOptError:
-		usage()
-		sys.exit()
-
-	user = None	# initialize so we can tell if it has been updated
-
-	for opt,arg in opts:
-		if opt in ["-h","--help"]:
-			usage()
-			sys.exit(1)
-		elif opt in ["-u","--user"]:
-			user = arg
-
-	if user==None:	# make sure user has been updated
-		usage()
-		sys.exit()
+			print "Values entered were not interpretable as numbers."
+			print item, "not added to master list."
+			sys.exit()
 	
-	owned_file = "%s_owned.txt" % user
-	if not os.path.isfile(owned_file): # make sure owned file exists
-		create_owned_file(owned_file)
-		sys.exit()
+	def read_owned_file(self):
+		with open(self.owned_file, 'r') as f:
+			for line in f:
+				l = line.strip()
+				if l[0] == '#':
+					category = l[1:]
+				elif category == "HELMS":
+					self.owned_helms.append(l)
+				elif category == "CHESTS":
+					self.owned_chests.append(l)
+				elif category == "GAUNTLETS":
+					self.owned_gauntlets.append(l)
+				elif category == "GREAVES":
+					self.owned_greaves.append(l)
+				else:
+					raise IOError
 	
-	try:
-		helms,chests,gauntlets,greaves = read_owned_file(owned_file)
-	except IOError:
-		print "Error: something went wrong reading", owned_file
-	
-	a = Armor()
-	build_armor_dict(a,helms,"helms")	
-	build_armor_dict(a,chests,"chests")
-	build_armor_dict(a,gauntlets,"gauntlets")
-	build_armor_dict(a,greaves,"greaves")
+	def create_owned_file(self):
+		create = raw_input("%s was not found. Would you like it to be created for you? " % self.owned_file)
+		if create in ["yes","y"]:
+			with open(self.owned_file, "w") as f:
+				f.write("#HELMS{0}No Helm{0}#CHESTS{0}No Chest{0}#GAUNTLETS{0}No Gauntlets{0}#GREAVES{0}No Greaves".format(os.linesep))
+			print self.owned_file, "has been created."
+		else:
+			print "Cannot continue without", self.owned_file
 
-	print a.get_type("helms")	
+	
+	def usage(self):
+		print "USAGE NOT YET IMPLIMENTED"
+	
+	def parse_args(self):
+		try:
+			opts,args = getopt.getopt(self.args, "hu:", ["help","user="])
+		except getopt.GetOptError:
+			self.usage()
+			sys.exit()
+		for opt,arg in opts:
+			if opt in ["-h","--help"]:
+				usage()
+				sys.exit(1)
+			elif opt in ["-u","--user"]:
+				self.user = arg
+
+		if self.user==None:	# make sure user has been updated
+			self.usage()
+			sys.exit()
 
 if __name__=="__main__":
-	main(sys.argv[1:])
+	Main(sys.argv[1:])
